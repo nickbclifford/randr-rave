6a028df06592200ff4679c6e5b5e6cc83f8f1a95
Nick Clifford <nick@nickclifford.me>
2020-03-03 21:52:12 -0600

    get ALSA configuration working

--- alsa.c
+++ alsa.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <alsa/asoundlib.h>
 
+#define SAMPLE_RATE 44100
+#define CHANNELS 2
+
 int main(int argc, char** argv) {
     if (argc < 2) {
         fprintf(stderr, "specify an ALSA device to capture from\n");
@@ -15,16 +18,44 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    printf("state %d\n", snd_pcm_state(handle));
-
     snd_pcm_hw_params_t* params;
-    if ((err = snd_pcm_hw_params_current(handle, params)) < 0) {
-        fprintf(stderr, "error getting device parameters: %s\n", snd_strerror(err));
+    if ((err = snd_pcm_hw_params_malloc(&params)) < 0) {
+        fprintf(stderr, "error allocating device parameters: %s\n", snd_strerror(err));
+        return 1;
+    }
+
+    if ((err = snd_pcm_hw_params_any(handle, params)) < 0) {
+        fprintf(stderr, "error initializing device parameters: %s\n", snd_strerror(err));
+        return 1;
+    }
+    
+    if ((err = snd_pcm_hw_params_set_access(handle, params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
+        fprintf(stderr, "error setting access: %s\n", snd_strerror(err));
+        return 1;
+    }
+
+    if ((err = snd_pcm_hw_params_set_format(handle, params, SND_PCM_FORMAT_S16_LE)) < 0) {
+        fprintf(stderr, "error setting format: %s\n", snd_strerror(err));
+        return 1;
+    }
+
+    int direction = 0;
+    if ((err = snd_pcm_hw_params_set_rate(handle, params, SAMPLE_RATE, direction)) < 0) {
+        fprintf(stderr, "error setting sample rate: %s\n", snd_strerror(err));
+        return 1;
+    }
+
+    if ((err = snd_pcm_hw_params_set_channels(handle, params, CHANNELS)) < 0) {
+        fprintf(stderr, "error setting channels: %s\n", snd_strerror(err));
+        return 1;
+    }
+
+    if ((err = snd_pcm_hw_params(handle, params)) < 0) {
+        fprintf(stderr, "error setting device parameters: %s\n", snd_strerror(err));
         return 1;
     }
 
     snd_pcm_uframes_t frames;
-    int direction;
     if ((err = snd_pcm_hw_params_get_period_size(params, &frames, &direction)) < 0) {
         fprintf(stderr, "error getting period size: %s\n", snd_strerror(err));
         return 1;
@@ -32,6 +63,7 @@ int main(int argc, char** argv) {
 
     printf("%d frames, sub unit direction %d\n", frames, direction);
 
+    snd_pcm_hw_params_free(params);
     snd_pcm_close(handle);
 
     return 0;
